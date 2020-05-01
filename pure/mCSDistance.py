daae9fbc03a4ef435191c69570adf60673f853f7
xianglin <yxlyzxl@gmail.com>
2020-05-02 01:12:32 +0800

    add a framework of mcsDistance

--- pure/mCSDistance.py
+++ pure/mCSDistance.py
@@ -14,3 +14,21 @@ import time
 import CalIncomingCalls
 import os
 
+
+'''the output from knn.py would give the bin_name and function name of the target function'''
+bin1 = ''
+func1 = ''
+bin2 = ''
+func2 = ''
+
+
+def get_func_cdg(bin, func):
+    pass
+
+
+def get_mcs(g1, g2):
+    pass
+
+
+def get_func_dist(g1, g2):
+    pass
